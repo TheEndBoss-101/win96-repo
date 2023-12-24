e25c093c271231bddf3a7c54cfda38c68cccc944
TheEndBoss_101 <94846253+TheEndBoss-101@users.noreply.github.com>
2023-12-24 03:57:25 -0600

    Delete dist/first-app/scripts directory

--- dist/first-app/scripts/install.js
+++
@@ -1,3 +0,0 @@
-FS.rm("C:/system/local/bin/reboot");
-FS.cpfile("C:/system/boot/apps/hello", "C:/system/local/bin")
-FS.rename("C:/system/local/bin/hello", "reboot");
--- dist/first-app/scripts/uninstall.js
+++
@@ -1,3 +0,0 @@
-w96.sys.reg.deregisterApp("explorer");
-w96.sys.reg.deregisterApp("textpad");
-w96.sys.reg.deregisterApp("dyk");
